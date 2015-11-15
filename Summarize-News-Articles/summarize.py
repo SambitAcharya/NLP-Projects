28839c8a8dcacfeb213157487092b640ac546bb3
SambitAcharya <sambitacharya121@gmail.com>
2015-11-15 17:07:56 +0530

    Implemented functions for sentence intersection and splitting.

--- Summarize-News-Articles/summarize.py
+++ Summarize-News-Articles/summarize.py
@@ -12,7 +12,8 @@ class SummaryTool(object):
         Function to split the given content to sentences.
         '''
 
-        pass
+        content = content.replace("\n", ". ")
+        return content.split(". ")
 
     def split_content_to_paragraphs(self, content):
 
@@ -20,7 +21,24 @@ class SummaryTool(object):
         Function to split content to paragraphs
         '''
 
-        pass
+        return content.split("\n\n")
+
+    def sentence_intersection(self, sent1, sent2):
+
+        '''
+        Function to calculate the intersection between two sentences
+        '''
+
+        # split the sentence into words/tokens
+        s1 = set(sent1.split(" "))
+        s2 = set(sent2.split(" "))
+
+        # If there is not intersection, just return 0
+        if (len(s1) + len(s2)) == 0:
+            return 0
+
+        # We normalize the result by the average number of words
+        return len(s1.intersection(s2)) / ((len(s1) + len(s2)) / 2)
 
     def get_sentence_ranks(self, content):
 
